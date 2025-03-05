93b204e9b51edf5a77284be7797e9b929d1a9324
Lebbe89 <lebbewow@gmx.de>
2025-03-05 19:12:08 +0100

    hi

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,6 +4,8 @@ from .serializers import HorseSerializer, EmployeeSerializer, FoodSerializer
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
 from rest_framework import viewsets
+from django.db.models import Q
+from django.http import JsonResponse
 
 class HorseViewSet(viewsets.ModelViewSet):
     queryset = Horse.objects.all()
@@ -20,18 +22,12 @@ class FoodViewSet(viewsets.ModelViewSet):
     serializer_class = FoodSerializer
     permission_classes = [AllowAny]
 
-# class EmployeeDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = Employee.objects.all()
-#     serializer_class = EmployeeSerializer
-#     permission_classes = [IsAuthenticated]
+def search_view(request):
+    query = request.GET.get('query', '')
+    
+    horses = Horse.objects.filter(Q(name__icontains=query) | Q(breed__icontains=query))
+    employees = Employee.objects.filter(Q(name__icontains=query) | Q(role__icontains=query))
+    food = Food.objects.filter(Q(name__icontains=query) | Q(type__icontains=query))
 
-# class HorseDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = Horse.objects.all()
-#     serializer_class = HorseSerializer
-#     permission_classes = [IsAuthenticated]
-
-# class FoodDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = Food.objects.all()
-#     serializer_class = FoodSerializer
-#     permission_classes = [IsAuthenticated]
-
+    results = list(horses.values()) + list(employees.values()) + list(food.values())
+    return JsonResponse(results, safe=False)
